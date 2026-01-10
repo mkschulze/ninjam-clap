5ba7c7eaa84d0d58dbde1e4e354d6aad1d27b06e
altarofwisdom <122935531+altarofwisdom@users.noreply.github.com>
2026-01-10 20:43:54 +0100

    chore: remove build artifacts and system files from repo

--- build-release/libs/clap/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/yoto/JUCEDEV/ninjam-clap/libs/clap
-# Build directory: C:/Users/yoto/JUCEDEV/ninjam-clap/build-release/libs/clap
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
